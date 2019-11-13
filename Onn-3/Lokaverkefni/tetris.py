3e1c6395bc7071bc9a0e6468639d7f0ba7e6c1a7
Gummy27 <brimir27@outlook.com>
2019-11-13 18:53:27 +0000

    Update

--- Onn-3/Lokaverkefni/tetris.py
+++ Onn-3/Lokaverkefni/tetris.py
@@ -5,23 +5,66 @@ import csv
 
 shapes = []
 with open('shapes.csv', 'r') as file:
-    listi = list(csv.reader(file, delimiter='-'))
+    listi = list(csv.reader(file, delimiter=' '))
     for shape in listi:
         shapes.append(list(csv.reader(shape)))
 
 class Shape():
     def __init__(self):
         self.shape = choice(shapes)
+        self.moving = True
+
+        self.calculateWidthHeight()
+
+    def rotate(self):
+        size_of_matrix = len(self.shape)
+        matrix = []
+        for x in range(size_of_matrix):
+            matrix.append([])
+            for i in range(size_of_matrix):
+                matrix[x].append('*')
+        print(matrix)
+
+        for x in range(size_of_matrix):
+            for y in range(size_of_matrix):
+                matrix[x][y] = self.shape[::-1][y][x]
+
+        self.shape = matrix
+        self.calculateWidthHeight()
+
+    def calculateWidthHeight(self):
+        loop = len(self.shape)
+        self.height, self.width = 0, 0
+        for x in range(loop):
+            highest, widest = 0, 0
+            for i in range(loop):
+                if self.shape[i][x] == '#':
+                    highest += 1
+                if self.shape[x][i] == '#':
+                    widest += 1
+            if self.height < highest:
+                self.height = highest
+
+            if self.width < widest:
+                self.width = widest
+
+        self.height *= size
+
 
     def drawShape(self, x, y):
         win.fill((0, 0, 0))
         for yy, listi in enumerate(self.shape):
-            print(listi)
             for xx, bool in enumerate(listi):
-                if bool == '1':
+                if bool == '#':
                     pygame.draw.rect(win, (255, 255, 255), [x + size * xx, y + size * yy, size, size], 0)
         pygame.display.update()
 
+    def collision(self, x, y):
+        if y + size + self.height < 500 - size:
+            return True
+        else:
+            self.moving = False
+            return False
 
 # your code
 start_time_down = time()
@@ -31,45 +74,61 @@ pygame.init()
 pygame.font.init()
 
 size = 25
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((500, 475))
 
 pygame.display.set_caption("First Game")
 
 
 running = True
-x, y = 100, 100
-pressed = False
 
-player = Shape()
-
-player.drawShape(x, y)
-
-pygame.display.update()
 while running:
-    for event in pygame.event.get():
-         if event.type == pygame.QUIT:
-             running = False
+    x, y = 100, 100
 
-    if time() - start_time_down > 0.8:
-        player.drawShape(x, y)
+    player = Shape()
+    player.drawShape(x, y)
+    pygame.display.update()
+    while player.moving:
+        for event in pygame.event.get():
+             if event.type == pygame.QUIT:
+                 running = False
 
-        start_time_down = time()
-        y += size
+        if time() - start_time_down > 1.5:
+            if player.collision(x, y):
+                y += size
+                player.drawShape(x, y)
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and time() - start_time_move > 0.2:
-        x -= size
+            start_time_down = time()
 
-        player.drawShape(x, y)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT] and time() - start_time_move > 0.2:
+            x -= size
 
-        start_time_move = time()
-        start_time_down = time()
+            player.drawShape(x, y)
 
-    if keys[pygame.K_RIGHT] and time() - start_time_move > 0.2:
-        x += size
+            start_time_move = time()
+            start_time_down = time()
 
-        player.drawShape(x, y)
+        if keys[pygame.K_RIGHT] and time() - start_time_move > 0.2:
+            x += size
 
-        start_time_move = time()
-        start_time_down = time()
+            player.drawShape(x, y)
+
+            start_time_move = time()
+            start_time_down = time()
+
+        if keys[pygame.K_UP] and time() - start_time_move > 0.2:
+            player.rotate()
+            player.drawShape(x, y)
+
+            start_time_move = time()
+            start_time_down = time()
+
+        if keys[pygame.K_DOWN] and time() - start_time_move > 0.2:
+            if player.collision(x, y):
+                y += size
+                player.drawShape(x, y)
+
+            start_time_down = time()
+            start_time_move = time()
+
 
--- Onn-3/Skilaverkefni_6/Teningar.py
+++ Onn-3/Skilaverkefni_6/Teningar.py
@@ -3,17 +3,18 @@ from random import randint
 
 pygame.init()
 pygame.font.init()
+
 class Dice():
     def __init__(self, face):
         self.sprite = pygame.image.load('sd'+str(face)+'.png')
         self.face = face
 
     def throw(self, play=False):
-        if play:
-            self.face = 0
-        else:
-            self.face = randint(1, 6)
-        self.sprite = pygame.image.load('sd'+str(self.face)+'.png')
+        nr = randint(1, 6)
+        self.face = nr
+        image = 'sd'+str(nr)+'.png'
+
+        self.sprite = pygame.image.load(image)
 
 class Die():
     def __init__(self):
@@ -26,15 +27,25 @@ class Die():
             dieB = []
         self.die = die
 
-    def throwDie(self, play=False):
-        print("yes")
+    def throwAllDie(self):
         for x, player in enumerate(self.die):
             for y, dice in enumerate(player):
-                if x != 1 and y != 5:
-                    dice.throw()
-                elif play:
-                    dice.throw()
-        pygame.display.update()
+                dice.throw()
+        self.drawGraphics()
+
+    def throwAllPlayer(self):
+        for dice in self.die[1]:
+            dice.throw()
+        self.drawGraphics()
+
+    def throwOnePlayer(self):
+        dice = self.die[1][4]
+        dice.throw()
+        self.drawGraphics()
+        for player in self.die:
+            for score in player:
+                print("Score:", score.face)
+
 
     def drawGraphics(self):
         for y, player in enumerate(self.die):
@@ -47,13 +58,23 @@ class Die():
                         win.blit(pygame.image.load('sd0.png'), (x*100+5, y*100+50))
                 else:
                     win.blit(dice.sprite, (x * 100 + 5, y * 100 + 50))
+        pygame.display.update()
+
+    def score(self):
+        finalScore, summa = [], 0
+        for player in self.die:
+            for score in player:
+                print(score.face)
+                summa += int(score.face)
+            finalScore.append(summa)
+            summa = 0
+        return finalScore
+
 
 win = pygame.display.set_mode((500, 500))
 active = Die()
 
-myfont = pygame.font.SysFont('Comic Sans MS', 60)
-
-# 500 - 40: 460
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
 cords, hitbox = 0, []
 messeges = ['Play', 'All', 'One']
@@ -69,25 +90,22 @@ pygame.display.update()
 
 hovering = 3
 running = True
+played = False
 holding = False
 while running:
     for event in pygame.event.get():
          if event.type == pygame.QUIT:
              running = False
-    keys = pygame.key.get_pressed()
 
+    keys = pygame.key.get_pressed()
     if keys[pygame.K_SPACE]:
         active.throwDie()
         active.drawGraphics()
         pygame.display.update()
 
     mpos = pygame.mouse.get_pos()
-    # [[50, 300], [150, 50]]
     for index, cords in enumerate(hitbox):
-        x1 = cords[0][0]
-        x2 = cords[1][0]
-        y1 = cords[0][1]
-        y2 = cords[1][1]
+        x1, x2, y1, y2 = cords[0][0], cords[1][0], cords[0][1], cords[1][1]
         if x2 > mpos[0] > x1 and y2 > mpos[1] > y1:
             if hovering == 3:
                 pygame.draw.rect(win, ([0, 255, 255]), [x1, y1, 100, 50], 0)
@@ -97,10 +115,7 @@ while running:
                 hovering = index
         elif hovering != 3 and hovering == index:
             cords = hitbox[hovering]
-            x1 = cords[0][0]
-            x2 = cords[1][0]
-            y1 = cords[0][1]
-            y2 = cords[1][1]
+            x1, x2, y1, y2 = cords[0][0], cords[1][0], cords[0][1], cords[1][1]
             pygame.draw.rect(win, ([255, 255, 255]), [x1, y1, 100, 50], 0)
             text = myfont.render(messeges[index], False, (0, 0, 0))
             win.blit(text, (x1, y1))
@@ -108,24 +123,48 @@ while running:
             hovering = 3
 
     click = pygame.mouse.get_pressed()[0]
-    if click and not holding:
-        holding = True
+    if click:
         for index, cords in enumerate(hitbox):
-            x1 = cords[0][0]
-            x2 = cords[1][0]
-            y1 = cords[0][1]
-            y2 = cords[1][1]
+            x1, x2, y1, y2 = cords[0][0], cords[1][0], cords[0][1], cords[1][1]
             if x2 > mpos[0] > x1 and y2 > mpos[1] > y1:
                 # Spila
-                if index == 0:
-                    active.throwDie()
+                if index == 0 and not played:
+                    active.throwAllDie()
+                    played = True
                 # All
                 elif index == 1:
-                    pass
+                    active.win = True
+                    active.throwAllPlayer()
+                    running = False
                 # One
                 elif index == 2:
-                    pass
+                    active.win = True
+                    active.throwOnePlayer()
+                    running = False
 
+computer, player = active.score()
+myfont = pygame.font.SysFont('Comic Sans MS', 60)
+win.fill((0, 0, 0))
+
+if computer > player:
+    text = myfont.render('Þú tapaðir!', False, (255, 255, 255))
+    win.blit(text, (80, 200))
+
+elif computer < player:
+    text = myfont.render('Þú vannst!', False, (255, 255, 255))
+    win.blit(text, (80, 200))
+
+elif computer == player:
+    text = myfont.render('Jafntefli!', False, (255, 255, 255))
+    win.blit(text, (80, 200))
+
+pygame.display.update()
+
+running = True
+while running:
+    for event in pygame.event.get():
+         if event.type == pygame.QUIT:
+             running = False
 
 
 
