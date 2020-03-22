a98337b52ba72b5639da0ea7b92dd60437af2d3f
Gummy27 <brimir27@outlook.com>
2020-03-22 10:11:27 +0000

    Forritunarkeppni 2020 bætt við

--- Forritunarkeppnir/2020/BidEndalausBid.py
+++ Forritunarkeppnir/2020/BidEndalausBid.py
@@ -1,1 +1,12 @@
-print("hello owlrd")
+fyrsta = list(map(int, input().split(":")))
+annad = list(map(int, input().split(":")))
+
+if annad[0] < fyrsta[0]:
+    minutu = (24 - fyrsta[0] + annad[0])*60
+else:
+    minutu = (annad[0] - fyrsta[0]) * 60
+
+minutu += annad[1] - fyrsta[1]
+
+print(minutu)
+
